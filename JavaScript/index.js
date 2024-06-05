077ca81bd3d4ce7604e25a8518b28e3c0594d8e2
Joao Victor <victorsrodrigues99@gmail.com>
2024-06-04 23:03:34 -0300

    :lipstick: Configurando seção home

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,3 +2,5 @@ document.getElementById('navbar-toggle').addEventListener('click', function () {
     const menu = document.getElementById('navbar-menu');
     menu.classList.toggle('show');
   });
+
+  
