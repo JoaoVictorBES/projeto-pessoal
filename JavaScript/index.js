fb59ac0fa885269c0908452f9b70bef477f7330c
Joao Victor <victorsrodrigues99@gmail.com>
2024-06-15 19:07:23 -0300

    :dizzy: Configurando section home

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,4 +3,23 @@ document.getElementById('navbar-toggle').addEventListener('click', function () {
     menu.classList.toggle('show');
   });
 
-  
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+    const body = document.body;
+
+    // Verifica se o modo escuro está ativado no localStorage
+    if (localStorage.getItem('dark-mode') === 'true') {
+        body.classList.add('dark-mode');
+    }
+
+    // Alterna o modo escuro/claro ao clicar no botão
+    toggleButton.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        // Salva a preferência no localStorage
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'true');
+        } else {
+            localStorage.setItem('dark-mode', 'false');
+        }
+    });
+});
