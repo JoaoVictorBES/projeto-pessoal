badb791efda29087f15f3238e8379ffb4f58162d
Joao Victor <victorsrodrigues99@gmail.com>
2024-11-05 22:46:53 -0300

    Atualizando link de referência github

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,4 @@
-document.querySelector('.header-ham-logo-menu').addEventListener('click', function() {
-    document.querySelector('.header-menu-ham').style.display = 'block';
-    document.querySelector('.header-ham-logo-menu').style.display = 'none';
-    document.querySelector('.header-ham-logo-menu-close').style.display = 'block';
-  });
-  
-  document.querySelector('.header-ham-logo-menu-close').addEventListener('click', function() {
-    document.querySelector('.header-menu-ham').style.display = 'none';
-    document.querySelector('.header-ham-logo-menu').style.display = 'block';
-    document.querySelector('.header-ham-logo-menu-close').style.display = 'none';
-  });
+function toggleMenu() {
+  var menu = document.querySelector('.header-main');
+  menu.classList.toggle('show');
+}
